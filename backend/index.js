9a2501e3942ae45a9b2a9d8d2fbc2f1887352343
Sohan Rajput <sohanrajput1803@gmail.com.com>
2026-02-02 21:17:17 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors')  // without cors the frontend cannot request the backend, as it will reject the req bcz the req is coming from diff port
+const AuthRouter = require('./routes/AuthRouter')
+
+require('dotenv').config();
+require('./models/db');
+
+const PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.json()) // will conver the json content into javascript object which can be accessed through req.body.___
+app.use(cors())  // allows req from diff ports
+app.use('/auth', AuthRouter)
+
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`)
+})
