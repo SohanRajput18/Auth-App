7d94c2238365272ae0df942992dfb99a706b0e57
Sohan Rajput <sohanrajput1803@gmail.com.com>
2026-02-09 20:31:50 +0530

    added comments in main.jsx and also css

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,12 +3,13 @@
 
 
 import React from 'react'
-import { createRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom/client'  // connects React code to the actual web browser DOM
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'  // This is the library that enables page navigation (routing) in React apps
+import 'react-toastify/ReactToastify.css'  // react-toastify is a popular library for showing notification popups (toasts), like: “Login successful”, “Item added to cart”, “Error occurred”
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render(  // Finds a real HTML element on your page, Attaches React to it, Creates the “root container” where your whole React app will live
   <React.StrictMode>
     <BrowserRouter>
       <App />
@@ -16,8 +17,7 @@ createRoot(document.getElementById('root')).render(
   </React.StrictMode>
 )
 
+//<React.StrictMode> is used to to help you find potential problems and bad practices in your code early.
 
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,7 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {Link} from 'react-router-dom'
+import {ToastContainer} from 'react-toastify'
 
 export default function Signup() {
+    const [signupInfo, setSignupInfo] = useState({
+        name: '',
+        email: '',
+        password: ''
+    })
+    const hangleChange = (e) => {
+        const {name, value} = e.target;
+        console.log(name, value);
+        const copySignupInfo = {...signupInfo};
+        copySignupInfo[name] = value;
+        setSignupInfo(copySignupInfo);
+    }
+
+    const handleSignup = (e) => {
+        e.preventDefault();
+    }
+
     return (
-        <div>Signup</div>
+        <div className='container'>
+            <h1>Login</h1>
+            <form onSubmit={handleSignup}>
+                <div>
+                    <label htmlFor="name">Name</label>
+                    <input 
+                        onChange={hangleChange}
+                        type="text" 
+                        name='name'
+                        autoFocus
+                        placeholder='Enter your name'
+                        value = {signupInfo.name}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="email">Email</label>
+                    <input 
+                        onChange={hangleChange}
+                        type="email" 
+                        name='email'
+                        autoFocus
+                        placeholder='Enter your email'
+                        value = {signupInfo.email}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password">Password</label>
+                    <input 
+                        onChange={hangleChange}
+                        type="password" 
+                        name='password'
+                        placeholder='Enter your password'
+                        value = {signupInfo.password}
+                    />
+                </div>
+                <button type='submit'>Signup</button>
+                <span>Already have an account ? 
+                    <Link to='/login'>Login</Link>
+                </span>
+            </form>
+            <ToastContainer />
+        </div>
     )
 }
