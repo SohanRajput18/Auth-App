9b811472f9f80d770a96a76b43b5afb2f7f69bc1
Sohan Rajput <sohanrajput1803@gmail.com.com>
2026-02-10 15:15:51 +0530

    completed sighup part

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
 import {ToastContainer} from 'react-toastify'
+import { handleError, handleSuccess } from '../utils';
 
 export default function Signup() {
     const [signupInfo, setSignupInfo] = useState({
@@ -8,6 +9,9 @@ export default function Signup() {
         email: '',
         password: ''
     })
+
+    const navigate = useNavigate();
+
     const hangleChange = (e) => {
         const {name, value} = e.target;
         console.log(name, value);
@@ -16,13 +20,46 @@ export default function Signup() {
         setSignupInfo(copySignupInfo);
     }
 
-    const handleSignup = (e) => {
+    const handleSignup = async (e) => {
         e.preventDefault();
+        const {name, email, password} = signupInfo;
+        if(!name || !email || !password) {
+            return handleError('name, email or password are required')
+        }
+        try {
+            const url = "http://localhost:8080/auth/signup"
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(signupInfo)
+            })
+            const result = await response.json();
+            const {success, message, error} = result;
+            if(success) {
+                handleSuccess(message);
+                setTimeout(() => {
+                    navigate('/login')
+                }, 1000)
+            }
+            else if(error) {
+                const details = error?.details[0].message; // If password len is less than 4
+                handleError(details);
+            }
+            else if(!success) {
+                handleError(message);
+            }
+            console.log(result)
+        }
+        catch (err) {
+            handleError(err);
+        }
     }
 
     return (
         <div className='container'>
-            <h1>Login</h1>
+            <h1>SignUp</h1>
             <form onSubmit={handleSignup}>
                 <div>
                     <label htmlFor="name">Name</label>
